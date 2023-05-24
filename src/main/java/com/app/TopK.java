89addd0dffab09e4ef2b50955a60f2edadde196f
San <San>
2023-05-24 08:02:07 -0700

    Refactor and add tests

--- src/main/java/com/app/TopK.java
+++ src/main/java/com/app/TopK.java
@@ -1,6 +1,7 @@
 package com.app;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.List;
@@ -11,26 +12,45 @@ import java.util.Set;
 
 public class TopK {
     public List<HeavyHitter> getTopK(List<String> events, Integer k) {
+        if (events == null || events.isEmpty()) {
+            throw new IllegalArgumentException("events cannot be empty");
+        }
+
+        if (k > events.size()) {
+            throw new IllegalArgumentException("No sufficient events");
+        }
+
         Map<String, Integer> map = new Hashtable<String, Integer>();
 
         for (String event : events) {
             map.put(event, map.getOrDefault(event, 0) + 1);
         }
 
-        PriorityQueue<HeavyHitter> queue = new PriorityQueue<HeavyHitter>(Comparator.comparing(e->e.getFrequency()));
+        PriorityQueue<HeavyHitter> queue = addTopEventsToQueue(map, k);
+        return createListOfQueueElements(queue);
+    }
+
+    private PriorityQueue<HeavyHitter> addTopEventsToQueue(Map<String, Integer> map, Integer k) {
+        PriorityQueue<HeavyHitter> queue = new PriorityQueue<HeavyHitter>(Comparator.comparing(e -> e.getFrequency()));
 
         final Set<Entry<String, Integer>> entrySet = map.entrySet();
 
         for (Entry<String, Integer> entry : entrySet) {
             queue.add(new HeavyHitter(entry.getKey(), entry.getValue()));
-            if(queue.size()>k) {
+            if (queue.size() > k) {
                 queue.poll();
             }
         }
+        return queue;
+    }
 
-        List<HeavyHitter> list = new ArrayList<>(k);
+    private List<HeavyHitter> createListOfQueueElements(PriorityQueue<HeavyHitter> queue) {
+        List<HeavyHitter> heavyHitters = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            heavyHitters.add(queue.poll());
+        }
 
-        queue.forEach(t -> list.add(t));
-        return list;
+        Collections.reverse(heavyHitters);
+        return heavyHitters;
     }
 }
--- src/test/java/com/app/TopKTest.java
+++ src/test/java/com/app/TopKTest.java
@@ -2,10 +2,13 @@ package com.app;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+
 public class TopKTest {
     TopK topK;
 
@@ -16,31 +19,56 @@ public class TopKTest {
 
     @Test
     public void testTopThree() {
-        List<String> events = Arrays.asList(new String[]{
-            "A", "A", "A", 
-            "B", "B", "B", "B",
-            "K", "K", "K", "K", "K", "K", 
-            "C", "C", "C", "C", "C", 
-            "Y"
+        List<String> events = Arrays.asList(new String[] {
+                "A", "A", "A",
+                "B", "B", "B", "B",
+                "K", "K", "K", "K", "K", "K",
+                "C", "C", "C", "C", "C",
+                "Y"
         });
         List<HeavyHitter> heavyHitters = topK.getTopK(events, 3);
         assertTrue("There are 3 heavy hitters", heavyHitters.size() == 3);
 
-        for(HeavyHitter heavyHitter : heavyHitters) {
+        for (HeavyHitter heavyHitter : heavyHitters) {
+            System.out.printf("%s - %d\n", heavyHitter.getId(), heavyHitter.getFrequency());
             switch (heavyHitter.getId()) {
-                case "B" : {
-                    assertTrue("4 B", heavyHitter.getFrequency()==4);
+                case "B": {
+                    assertTrue("4 B", heavyHitter.getFrequency() == 4);
                     break;
                 }
-                case "C" : {
-                    assertTrue("5 C", heavyHitter.getFrequency()==5);
+                case "C": {
+                    assertTrue("5 C", heavyHitter.getFrequency() == 5);
                     break;
                 }
-                case "K" : {
-                    assertTrue("6 K", heavyHitter.getFrequency()==6);
+                case "K": {
+                    assertTrue("6 K", heavyHitter.getFrequency() == 6);
                     break;
                 }
-            } 
+            }
         }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullEvents() {
+        topK.getTopK(null, 3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoEvents() {
+        topK.getTopK(new ArrayList<>(), 3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSufficientEvents() {
+        topK.getTopK(Arrays.asList(new String[] { "A" }), 3);
+    }
+
+    @Test
+    public void testSingleElement() {
+        List<String> events = Arrays.asList(new String[] { "A" });
+        List<HeavyHitter> heavyHitters = topK.getTopK(events, 1);
+        assertTrue("There is 1 heavy hitters", heavyHitters.size() == 1);
+        assertTrue("A", heavyHitters.get(0).getId() == "A");
+        assertTrue("Only 1 A", heavyHitters.get(0).getFrequency() == 1);
     }
 }
